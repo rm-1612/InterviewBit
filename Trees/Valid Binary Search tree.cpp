a108087ae5484fc995d43beb23ce3899a34b0994
rm-1612 <ruchika.modi.apm13@itbhu.ac.in>
2017-06-05 21:59:48 +0530

    Create Valid Binary Search tree.cpp

---
+++ Trees/Valid Binary Search tree.cpp
@@ -0,0 +1,56 @@
+/*
+
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node’s key.
+The right subtree of a node contains only nodes with keys greater than the node’s key.
+Both the left and right subtrees must also be binary search trees.
+Example :
+
+Input : 
+   1
+  /  \
+ 2    3
+
+Output : 0 or False
+
+
+Input : 
+  2
+ / \
+1   3
+
+Output : 1 or True 
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+bool isvalid(TreeNode* A,int min,int max)
+{
+    if(A==NULL)
+    return true;
+    if(A->val<min || A->val>max)
+    return false;
+    
+    return isvalid(A->left,min,A->val-1) && isvalid(A->right,A->val+1,max);
+}
+int Solution::isValidBST(TreeNode* A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    return isvalid(A,INT_MIN,INT_MAX);
+    
+}

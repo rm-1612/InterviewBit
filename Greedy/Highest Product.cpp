665f571bc3b6f2710b32840599919328c9bdb9f5
rm-1612 <ruchika.modi.apm13@itbhu.ac.in>
2017-06-06 02:23:06 +0530

    Create Highest Product.cpp

---
+++ Greedy/Highest Product.cpp
@@ -0,0 +1,27 @@
+/*
+
+Given an array of integers, return the highest product possible by multiplying 3 numbers from the array
+
+Input:
+
+array of integers e.g {1, 2, 3}
+ NOTE: Solution will fit in a 32-bit signed integer 
+Example:
+
+[0, -1, 3, 100, 70, 50]
+
+=> 70*50*100 = 350000
+
+*/
+
+int Solution::maxp3(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int n=A.size();
+    sort(A.begin(),A.end());
+    int result;
+    result=max(A[0]*A[1]*A[n-1],A[n-1]*A[n-2]*A[n-3]);
+    return result;
+}

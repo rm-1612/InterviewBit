3f5e622a9d8542eddd083193902d0b68854970cc
rm-1612 <ruchika.modi.apm13@itbhu.ac.in>
2017-06-05 21:31:20 +0530

    Create Number of 1 Bits.cpp

---
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -0,0 +1,30 @@
+/*
+
+Write a function that takes an unsigned integer and returns the number of 1 bits it has.
+
+Example:
+
+The 32-bit integer 11 has binary representation
+
+00000000000000000000000000001011
+so the function should return 3.
+
+Note that since Java does not have unsigned int, use long for Java
+
+*/
+
+int Solution::numSetBits(unsigned int A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int ctr=0;
+    while(A>0)
+    {
+        if(A%2!=0)
+        ++ctr;
+        A=A/2;
+        
+    }
+    return ctr;
+}

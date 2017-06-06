2ad6ad0508e08b2c29bc657b3a273899291a4b44
rm-1612 <ruchika.modi.apm13@itbhu.ac.in>
2017-06-07 02:19:32 +0530

    Create  Level Order.cpp

---
+++ Graphs/ Level Order.cpp
@@ -0,0 +1,66 @@
+/*
+
+Given a binary tree, return the level order traversal of its nodes’ values. (ie, from left to right, level by level).
+
+Example :
+Given binary tree
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+Also think about a version of the question where you are asked to do a level order traversal of the tree when depth of the tree is much greater than number of nodes on a level.
+
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<vector<int> > Solution::levelOrder(TreeNode* A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    vector<vector<int> > vec;
+    if(A==NULL)
+    return vec;
+    queue<TreeNode* > q;
+    q.push(A);
+    while(q.empty()==false)
+    {
+        int nodecount=q.size();
+        vector<int> v;
+        while(nodecount>0)
+        {
+            TreeNode* temp=q.front();
+            q.pop();
+            v.push_back(temp->val);
+            if(temp->left!=NULL)
+            {
+                q.push(temp->left);
+            }
+            if(temp->right!=NULL)
+            {
+                q.push(temp->right);
+            }
+            --nodecount;
+        }
+        vec.push_back(v);
+    }
+    return vec;
+}
